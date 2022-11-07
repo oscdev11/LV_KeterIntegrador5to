db0402bb22e8b62e0f85a7ac10d8c25223005b6a
EmmanuelTejedaTejeda <emmanuel.2d.28@gmail.com>
2022-11-07 14:24:12 -0600

    jdjssajja

---
+++ application/views/Plantas/detallePlanta.php
@@ -0,0 +1,13 @@
+<div class="container-fluid">
+    <br>
+    <div class="alert alert-success align-middle" role="alert" align="center"><h5>Detalles generales de planta: <b><?=$planta[0]->nombre ?></h5></div>
+    <br>
+</div>
+
+<div class="container"></div>
+
+<div class="container" align="center">
+    <div class="col-8">
+        
+    </div>
+</div>
