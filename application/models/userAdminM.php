15784c6032ba0e501dd92efd7708fc48acfaeaff
oscarLS-11 <oluna1103@gmail.com>
2022-11-09 13:38:40 -0600

    asdfasdf

--- application/models/userAdminM.php
+++ application/models/userAdminM.php
@@ -8,14 +8,14 @@
             return $query->result();
         }
 
-        //obtener detalles del usuario administrador
+        // obtener detalles del usuario administrador
         function getUserAdmin($id_UserAdmin){
             $this->db->where('id_UserAdmin', $id_UserAdmin);
             $query = $this->db->get('UserAdmin');
             return $query->result();
         }
 
-        //editar información de Color
+        // editar información de Color
         function updateUserAdmin($id_UserAdmin){
             $data = array(
                 'nombreUsuario' => $this->input->post('nombreUsuario'),
