5f412bf2d0f85988a2f11c31dfa8567629878dce
EmmanuelTejedaTejeda <emmanuel.2d.28@gmail.com>
2022-11-11 12:22:48 -0600

    dsas

--- application/views/Plantas/insertPlanta.php
+++ application/views/Plantas/insertPlanta.php
@@ -1,2 +1,10 @@
-<div></div>
-<div></div>
+<div class="conteiner-fluid">
+    <br>
+    <div class="alert alert-success align-middle" role="alert" align="center"><h5>Agregar Nueva Planta</h5></div>
+    </br>
+</div>
+
+<div class="conteiner">
+    <?php
+    ?>
+</div>
