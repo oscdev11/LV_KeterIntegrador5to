e53954bd31bb2bba8c831f90c3e6020cee154467
jorgeluis1122 <jorgeluissebastianh@gmail.com>
2022-11-12 12:27:03 -0600

    .

--- application/controllers/EmpleadosC.php
+++ application/controllers/EmpleadosC.php
@@ -12,5 +12,14 @@ class EmpleadosC extends CI_Controller
         $this->load->view('empleados/listaEmpleados.php', $data);
         $this->load->view('headers/footer.php');
     }
+    public function detalleEmpleado($id_Cliente){
+        $this->load->model('EmpleadosM');
+        $data['empleado'] = $this->EmpleadosM->getEmpleado($id_Empleado);
+
+        $this->load->view('headers/head.php');
+        $this->load->view('headers/menu.php');
+        $this->load->view('empleados/detalleEmpleado.php', $data);
+        $this->load->view('headers/footer.php');
+    }
 }
 ?>
