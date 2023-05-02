6e3f12534a20ea87979efaebf99ad6fe065b4a74
OscarLS <oscarLS-11>
2023-05-02 17:47:13 +0000

    MD5 logueo

--- application/models/UsuarioM.php
+++ application/models/UsuarioM.php
@@ -18,7 +18,7 @@ class UsuarioM extends CI_Model{
     public function validaUsuarioE($nombreUsuario, $contra){
 
         $this->db->where('nombreUsuario',$nombreUsuario);
-        $this->db->where('contra',$contra);
+        $this->db->where('contra',md5($contra));
         $q = $this->db->get('UserAdmin');
         if ($q->num_rows()>0) {
             return true;
